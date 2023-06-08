4eefd9c4386694013184eeb68baccbcf789d43ff
Andy Tran <transient.tran@gmail.com>
2023-06-07 20:54:38 -0400

    add generic to AppProps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth"
 
-const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps<{
+  session: Session;
+}>) => {
   return (
     <SessionProvider session={pageProps.session}>
     <Component {...pageProps} />
